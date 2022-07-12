6cb1c802e04b8efd0abdc0a2d6dbdbee4747b195
PatriciuMina <mina.patriciu@gmail.com>
2022-07-13 02:08:17 +0300

    first try of login and register

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  #filter_parameter_logging :password, :password_confirmation
+
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:name, :email, :password, :password_confirmation]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+
+  end
 end
+
+
